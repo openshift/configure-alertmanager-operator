aded82689a857af7d4cdedc84c5c197374673857
Lisa Seelye <18159+lisa@users.noreply.github.com>
2019-05-16 10:07:17 -0400

    SREP-1186: Adopt app-sre pipeline (#13)
    There are a several changes to the "vanilla" app-sre pipeline that have been implemented.
    
    First, many of the helper scripts have been lifted into Makefiles, in particlar, `functions.mk` has a defined function, `create_catalog_image` which creates the operator's Catalog image.
    
    Next, the Python script used to generate the CSV has been adopted and modified from the osd-operators-registry script. This version is more self-contained and hardened.
    
    The `yq` container image has been udpated to the official one (if there's a 404 on the curl, for example, the extant version 1.5 would fail).
    
    Calling the `create_catalog_image` function requires 8 inputs:
    
    1. Channel (the branch name in the 'operator bundle' repo, eg `staging`, or `production`)
    2. Bundle github name (eg `app-sre/saas-configure-alertmanager-operator-bundle`)
    3. Authentication token for `app` github user to push.
    4. Whether or not to remove any versions more recent than deployed hash (`true` or `false`)
    5. saasherder config github repo name (eg `app-sre/saas-hive`)
    6. saasherder config path (absolute within repo, eg `/$(OPERATOR_NAME)/$(OPERATOR_NAME).yaml`)
    7. relative path to bundle generator python script (eg `build/generate-operator-bundle.py`)
    8. Catalog registry quay.io organization name (eg `$(CATALOG_REGISTRY_ORGANIZATION)`)
    
    Thus, to build for `staging` and `production`, this Make target:
    
        .PHONY: build-catalog-image
        build-catalog-image:
        	$(call create_catalog_image,staging,app-sre/saas-configure-alertmanager-operator-bundle,$$APP_SRE_BOT_PUSH_TOKEN,false,app-sre/saas-hive,/$(OPERATOR_NAME)/$(OPERATOR_NAME).yaml,build/generate-operator-bundle.py,$(CATALOG_REGISTRY_ORGANIZATION))
        	$(call create_catalog_image,production,app-sre/saas-configure-alertmanager-operator-bundle,$$APP_SRE_BOT_PUSH_TOKEN,true,app-sre/saas-hive,/$(OPERATOR_NAME)/$(OPERATOR_NAME).yaml,build/generate-operator-bundle.py,$(CATALOG_REGISTRY_ORGANIZATION))
    
    The `standard.mk` Makefile has also had its `clean` target updated to delete the bundles and all local docker images:
    
        .PHONY: clean
        clean:
        	rm -rf ./build/_output bundles-staging bundles-production
        	docker rmi \
        		${OPERATOR_IMAGE_URI} \
        		${OPERATOR_IMAGE_URI_LATEST} \
        		quay.io/${CATALOG_REGISTRY_ORGANIZATION}/$(OPERATOR_NAME):staging-latest \
        		quay.io/${CATALOG_REGISTRY_ORGANIZATION}/$(OPERATOR_NAME):production-latest 2>/dev/null || true
    
    
    Signed-off-by: Lisa Seelye <lseelye@redhat.com>

---
+++ build/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)/..
+
+if [[ -z $IMAGE_REPOSITORY ]]; then
+  IMAGE_REPOSITORY=app-sre
+fi
+
+# Build the image
+
+make IMAGE_REPOSITORY=$IMAGE_REPOSITORY build
