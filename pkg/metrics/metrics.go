f66c8536a59064d5a1b22d62cff2e6e592863abf
Stefanie Forrester <sedgar@redhat.com>
2019-05-19 10:22:26 -0700

    Add metrics for operator
    Added metrics that will be scraped by Prometheus,
    and fixed regex in Pager Duty receiver.
    Solves https://github.com/openshift/configure-alertmanager-operator/issues/1 and https://github.com/openshift/configure-alertmanager-operator/issues/9

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -17,7 +17,10 @@ package metrics
 import (
 	"net/http"
 
+	"github.com/openshift/configure-alertmanager-operator/config"
+	alertmanager "github.com/openshift/configure-alertmanager-operator/pkg/types"
 	"github.com/prometheus/client_golang/prometheus"
+	corev1 "k8s.io/api/core/v1"
 )
 
 const (
@@ -26,13 +29,33 @@ const (
 )
 
 var (
-	metricPlaceholder = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "placeholder",
-		Help: "Placeholder",
+	metricPDSecretExists = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "pd_secret_exists",
+		Help: "Pager Duty secret exists",
+	}, []string{"name"})
+	metricDMSSecretExists = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "dms_secret_exists",
+		Help: "Dead Man's Snitch secret exists",
+	}, []string{"name"})
+	metricAMSecretExists = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "am_secret_exists",
+		Help: "AlertManager Config secret exists",
+	}, []string{"name"})
+	metricAMSecretContainsPD = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "am_secret_contains_pd",
+		Help: "AlertManager Config contains configuration for Pager Duty",
+	}, []string{"name"})
+	metricAMSecretContainsDMS = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "am_secret_contains_dms",
+		Help: "AlertManager Config contains configuration for Dead Man's Snitch",
 	}, []string{"name"})
 
 	metricsList = []prometheus.Collector{
-		metricPlaceholder,
+		metricPDSecretExists,
+		metricDMSSecretExists,
+		metricAMSecretExists,
+		metricAMSecretContainsPD,
+		metricAMSecretContainsDMS,
 	}
 )
 
@@ -56,8 +79,71 @@ func RegisterMetrics() error {
 	return nil
 }
 
-// UpdatePlaceholderGauge ...
-func UpdatePlaceholderGauge() {
+// UpdateSecretsMetrics updates all metrics related to the existance and contents of Secrets
+// used by configure-alertmanager-operator.
+func UpdateSecretsMetrics(list *corev1.SecretList, amconfig alertmanager.Config) {
 
-	metricPlaceholder.With(prometheus.Labels{"name": "pagerduty-operator"}).Set(float64(1))
+	// Default to false.
+	pdSecretExists := false
+	dmsSecretExists := false
+	amSecretExists := false
+	amSecretContainsPD := false
+	amSecretContainsDMS := false
+
+	// Update the metric if the secret is found in the SecretList.
+	for _, secret := range list.Items {
+		switch secret.Name {
+		case "pd-secret":
+			pdSecretExists = true
+		case "dms-secret":
+			dmsSecretExists = true
+		case "alertmanager-main":
+			amSecretExists = true
+		}
+	}
+
+	// Check for the presence of PD and DMS configs inside the AlertManager config and report metrics.
+	if amSecretExists {
+		if pdSecretExists {
+			for _, receiver := range amconfig.Receivers {
+				if receiver.Name == "pagerduty" {
+					amSecretContainsPD = true
+				}
+			}
+		}
+		if dmsSecretExists {
+			for _, receiver := range amconfig.Receivers {
+				if receiver.Name == "watchdog" {
+					amSecretContainsDMS = true
+				}
+			}
+		}
+	}
+
+	// Only set metrics once per run.
+	if pdSecretExists {
+		metricPDSecretExists.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(1))
+	} else {
+		metricPDSecretExists.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(0))
+	}
+	if dmsSecretExists {
+		metricDMSSecretExists.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(1))
+	} else {
+		metricDMSSecretExists.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(0))
+	}
+	if amSecretExists {
+		metricAMSecretExists.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(1))
+	} else {
+		metricAMSecretExists.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(0))
+	}
+	if amSecretContainsPD {
+		metricAMSecretContainsPD.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(1))
+	} else {
+		metricAMSecretContainsPD.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(0))
+	}
+	if amSecretContainsDMS {
+		metricAMSecretContainsDMS.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(1))
+	} else {
+		metricAMSecretContainsDMS.With(prometheus.Labels{"name": config.OperatorName}).Set(float64(0))
+	}
 }
