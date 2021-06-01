6bc27dbac53a675a8b83dc4890ec51bcd5fc5efe
Rick Rackow <rrackow@redhat.com>
2021-06-01 16:05:56 +0200

    Stop filtering based on `namespace` label

--- pkg/controller/secret/secret_controller.go
+++ pkg/controller/secret/secret_controller.go
@@ -171,9 +171,9 @@ func createPagerdutyRoute() *alertmanager.Route {
 
 		// https://issues.redhat.com/browse/OSD-3086
 		// https://issues.redhat.com/browse/OSD-5872
-		{Receiver: receiverPagerduty, MatchRE: map[string]string{"exported_namespace": alertmanager.PDRegex}, Match: map[string]string{"prometheus": "openshift-monitoring/k8s"}},
+		{Receiver: receiverPagerduty, MatchRE: map[string]string{Match: map[string]string{"prometheus": "openshift-monitoring/k8s"}},
 		// general: route anything in core namespaces to PD
-		{Receiver: receiverPagerduty, MatchRE: map[string]string{"namespace": alertmanager.PDRegex}, Match: map[string]string{"exported_namespace": "", "prometheus": "openshift-monitoring/k8s"}},
+		{Receiver: receiverPagerduty, MatchRE: map[string]string{Match: map[string]string{"exported_namespace": "", "prometheus": "openshift-monitoring/k8s"}},
 		// fluentd: route any fluentd alert to PD
 		// https://issues.redhat.com/browse/OSD-3326
 		{Receiver: receiverPagerduty, Match: map[string]string{"job": "fluentd", "prometheus": "openshift-monitoring/k8s"}},
