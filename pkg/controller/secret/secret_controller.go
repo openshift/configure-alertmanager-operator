bfff2c0320e9065ec3d982b7d9719e01ee189856
Dominic Finn <dofinn@redhat.com>
2021-07-02 11:44:40 +1000

    route openshift-operators-redhat to devnull

--- pkg/controller/secret/secret_controller.go
+++ pkg/controller/secret/secret_controller.go
@@ -129,6 +129,8 @@ func createPagerdutyRoute() *alertmanager.Route {
 		{Receiver: receiverNull, Match: map[string]string{"namespace": "openshift-operators"}},
 		// https://issues.redhat.com/browse/OSD-6505
 		{Receiver: receiverNull, Match: map[string]string{"exported_namespace": "openshift-operators"}},
+		// https://issues.redhat.com/browse/OSD-7653
+		{Receiver: receiverNull, Match: map[string]string{"namespace": "openshift-operators-redhat"}},
 		// https://issues.redhat.com/browse/OSD-3629
 		{Receiver: receiverNull, Match: map[string]string{"alertname": "CustomResourceDetected"}},
 		// https://issues.redhat.com/browse/OSD-3629
