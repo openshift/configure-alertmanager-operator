1727aa92bd40d5b194d7b0520543e4250c0eaa4e
Pooja Rani <prani@redhat.com>
2021-09-06 13:04:04 +0530

    Ignore openshift-storage namespace from alerting

--- pkg/controller/secret/secret_controller.go
+++ pkg/controller/secret/secret_controller.go
@@ -129,6 +129,8 @@ func createPagerdutyRoute() *alertmanager.Route {
 		{Receiver: receiverNull, Match: map[string]string{"namespace": "openshift-operators"}},
 		// https://issues.redhat.com/browse/OSD-7631
 		{Receiver: receiverNull, Match: map[string]string{"namespace": "openshift-pipelines"}},
+		// https://issues.redhat.com/browse/OSD-8337
+		{Receiver: receiverNull, Match: map[string]string{"namespace": "openshift-storage"}},
 		// https://issues.redhat.com/browse/OSD-7747
 		{Receiver: receiverNull, Match: map[string]string{"namespace": "openshift-cnv"}},
 		// https://issues.redhat.com/browse/OSD-6505
