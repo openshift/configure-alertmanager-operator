1678360205be6e7d6c343367821784f5c87384bc
klin <klin@redhat.com>
2022-11-23 10:29:53 -0500

    remove brackets

--- controllers/secret_controller.go
+++ controllers/secret_controller.go
@@ -386,9 +386,8 @@ func createPagerdutyRoute(namespaceList []string) *alertmanager.Route {
 	// https://issues.redhat.com/browse/OSD-13685
 	if config.IsFedramp() {
 		pagerdutySubroutes = append(pagerdutySubroutes,
-			[]*alertmanager.Route{
-				{Receiver: receiverNull, Match: map[string]string{"alertname": "ClusterOperatorDown", "name": "insights"}},
-			}...)
+			&alertmanager.Route{Receiver: receiverNull, Match: map[string]string{"alertname": "ClusterOperatorDown", "name": "insights"}},
+		)
 	}
 
 	for _, namespace := range namespaceList {
