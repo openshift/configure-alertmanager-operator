15db9c8f55880359f6a1734de1549670fb84e588
Katherine Lin <85188042+katherinelc321@users.noreply.github.com>
2022-12-13 13:32:45 -0500

    Silence Insights operator down alert in FedRAMP environment (#248)
    * silence insights in fr
    
    * only silence clusteroperatordown
    
    * remove line
    
    * remove brackets

--- controllers/secret_controller.go
+++ controllers/secret_controller.go
@@ -385,6 +385,14 @@ func createPagerdutyRoute(namespaceList []string) *alertmanager.Route {
 		{Receiver: receiverNull, Match: map[string]string{"alertname": "MultipleDefaultStorageClasses", "namespace": "openshift-cluster-storage-operator"}},
 	}
 
+	// Silence insights in FedRAMP until its made available in the environment
+	// https://issues.redhat.com/browse/OSD-13685
+	if config.IsFedramp() {
+		pagerdutySubroutes = append(pagerdutySubroutes,
+			&alertmanager.Route{Receiver: receiverNull, Match: map[string]string{"alertname": "ClusterOperatorDown", "name": "insights"}},
+		)
+	}
+
 	for _, namespace := range namespaceList {
 		pagerdutySubroutes = append(pagerdutySubroutes, []*alertmanager.Route{
 			// https://issues.redhat.com/browse/OSD-3086
