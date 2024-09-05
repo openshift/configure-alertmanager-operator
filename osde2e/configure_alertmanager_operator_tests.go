6abe35cb254b67cb737a74285114c0f36c4e4739
YiqinZhang <yiqzhang@redhat.com>
2024-09-05 11:49:25 -0400

    Add OSDe2e test harness scaffolding

---
+++ osde2e/configure_alertmanager_operator_tests.go
@@ -0,0 +1,13 @@
+// DO NOT REMOVE TAGS BELOW. IF ANY NEW TEST FILES ARE CREATED UNDER /osde2e, PLEASE ADD THESE TAGS TO THEM IN ORDER TO BE EXCLUDED FROM UNIT TESTS. //go:build osde2e
+//go:build osde2e
+// +build osde2e
+
+package osde2etests
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("configure-alertmanager-operator", func() {
+	//	 Add your tests
+})
